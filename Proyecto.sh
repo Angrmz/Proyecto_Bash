2c39a04643dbec2e9d56c9707c4499bc128e5ab8
Angelica Ramirez <a295866@uach.mx>
2021-10-27 11:28:32 -0600

    Se agregan menus al archivo Proyecto.sh

---
+++ Proyecto.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+Menu_1(){
+    echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:"
+    echo "1. SCRUM"
+    echo "2. X.P."
+    echo "3. Kanban"
+    echo "4. Crystal"
+    echo "5. Salir"
+    echo
+}
+
+Menu_2(){
+    echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:"
+    echo "1. Cascada"
+    echo "2. Espiral"
+    echo "3. Modelo V"
+    echo "4. Salir"
+    echo
+}
